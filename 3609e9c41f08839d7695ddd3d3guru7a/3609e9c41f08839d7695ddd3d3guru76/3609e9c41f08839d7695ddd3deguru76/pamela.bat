7b09bb2c15cf71ffe00a7c1fcf27c0a4b885c5aa
triandessinaga <ctriandes@gmail.com>
2021-03-01 17:19:56 +0700

    add file to git

---
+++ 3609e9c41f08839d7695ddd3d3guru7a/3609e9c41f08839d7695ddd3d3guru76/3609e9c41f08839d7695ddd3deguru76/pamela.bat
@@ -0,0 +1,5 @@
+@echo off
+start php D:\3609e9c41f08839d7695ddd3de6uru7h\3609e9c41f08839d7695ddd3deguru12\3609e9c41f08839d7695ddd3deguru76\3609e9c41f08839d7695ddd3deguru76\3609e9c41f08839d7695ddd3de6uru5h\artisan serve --host 0.0.0.0
+artisan serve
+Timeout /t 6/nobreak >nul
+start chrome http://localhost:8000/
